69fe5c42858a9d59de27ff254878400abe973556
Jesus29-dev <jeusguilermo@gmail.com>
2024-12-04 18:16:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,150 @@
-class Checklist:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.questions = []
-        self.answers = []
+from flask import Flask
 
-    def add_question(self, question):
-        self.questions.append(question)
+from flask import render_template
+from flask import request
+from flask import jsonify, make_response
 
-    def get_questions(self):
-        return self.questions
+import pusher
 
-class ChecklistFactory:
-    def create_checklist(self, type):
-        if type == "limpieza":
-            return ChecklistLimpieza()
-        elif type == "orden":
-            return ChecklistOrden()
-        else:
-            raise ValueError("Tipo de checklist no válido")
+import mysql.connector
+import mysql.connector.error
 
-class ChecklistLimpieza(Checklist):
-    def __init__(self):
-        super().__init__(1, "Checklist de Limpieza")
-        self.add_question("¿El piso está limpio?")
-        # ... otras preguntas
+import datetime
+import pytz
 
-class ChecklistOrden(Checklist):
-    def __init__(self):
-        super().__init__(2, "Checklist de Orden")
-        self.add_question("¿Las herramientas están en su lugar?")
-        # ... otras preguntas
+form flask_cors import CORS, cross_origin
+
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    con.close()
+
+    return render_template("app.html")
+
+@app.route("/alumnos")
+def alumnos():
+    con.close()
+
+    return render_template("alumnos.html")
+
+@app.route("/alumnos/guardar", methods=["POST"])
+def alumnosGuardar():
+    con.close()
+    matricula      = request.form["txtMatriculaFA"]
+    nombreapellido = request.form["txtNombreApellidoFA"]
+
+    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+# Código usado en las prácticas
+def notificarActualizacionTemperaturaHumedad():
+    pusher_client = pusher.Pusher(
+        app_id="1714541",
+        key="2df86616075904231311",
+        secret="2f91d936fd43d8e85a1a",
+        cluster="us2",
+        ssl=True
+    )
+
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
+
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT Id_Log, Temperatura, Humedad, DATE_FORMAT(Fecha_Hora, '%d/%m/%Y') AS Fecha, DATE_FORMAT(Fecha_Hora, '%H:%i:%s') AS Hora FROM sensor_log
+    ORDER BY Id_Log DESC
+    LIMIT 10 OFFSET 0
+    """)
+    registros = cursor.fetchall()
+
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/guardar", methods=["POST"])
+def guardar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id          = request.form["id"]
+    temperatura = request.form["temperatura"]
+    humedad     = request.form["humedad"]
+    fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
+    
+    cursor = con.cursor()
+
+    if id:
+        sql = """
+        UPDATE sensor_log SET
+        Temperatura = %s,
+        Humedad     = %s
+        WHERE Id_Log = %s
+        """
+        val = (temperatura, humedad, id)
+    else:
+        sql = """
+        INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora)
+                        VALUES (%s,          %s,      %s)
+        """
+        val =                  (temperatura, humedad, fechahora)
+    
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    notificarActualizacionTemperaturaHumedad()
+
+    return make_response(jsonify({}))
+
+@app.route("/editar", methods=["GET"])
+def editar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.args["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    SELECT Id_Log, Temperatura, Humedad FROM sensor_log
+    WHERE Id_Log = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    registros = cursor.fetchall()
+    con.close()
+
+    return make_response(jsonify(registros))
+
+@app.route("/eliminar", methods=["POST"])
+def eliminar():
+    if not con.is_connected():
+        con.reconnect()
+
+    id = request.form["id"]
+
+    cursor = con.cursor(dictionary=True)
+    sql    = """
+    DELETE FROM sensor_log
+    WHERE Id_Log = %s
+    """
+    val    = (id,)
+
+    cursor.execute(sql, val)
+    con.commit()
+    con.close()
+
+    notificarActualizacionTemperaturaHumedad()
+
+    return make_response(jsonify({}))
